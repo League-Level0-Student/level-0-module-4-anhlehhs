68cffb89c7db01bd6dda8cae28403ce2c76dad29
root <root@70a0c8b44149>
2021-05-18 22:26:42 +0000

    string

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -1,31 +1,38 @@
 package _03_char._2_string_methods;
 
+import javax.swing.JOptionPane;
 
 public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-String bob= new String("bob");
+String Jon= new String("Jon");
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-System.out.println(bob.charAt(2));
+System.out.println(Jon.charAt(1));
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-System.out.println(bob.length());
+System.out.println(Jon.length());
 		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-for (int i = 0; i < bob.length(); i++) {
-	System.out.println(bob.charAt(bob.length()-1));
+for (int i = 0; i < Jon.length(); i++) {
+	System.out.println(Jon.charAt(i));
 }
 		
+String input=JOptionPane.showInputDialog(null, "What character would you want to know?");
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+		char c = input.charAt(0);
+		for (int i = 0; i < Jon.length(); i++) {
+			if(Jon.charAt(i) == c) {
+				JOptionPane.showMessageDialog(null, "That character is at position " + i);
+			}
+		}
 	}
 }
 
--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -3,6 +3,8 @@ package _03_char._3_pi_aloud;
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class PiAloud {
 	/***********  SOUND ***************
 	 * Some computers are unable to play sounds. 
@@ -13,26 +15,40 @@ public class PiAloud {
 	
 	
 	// 1. Make a main method and make sure your program can run
-
+public static void main(String[] args) {
+	
+	
 	// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
-
+String pi=new String("3.14159265358979323846");
 	// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 	// Check the console shows 3.1
-
+//System.out.println(pi.charAt(0));
+//System.out.println(pi.charAt(1));
+//System.out.println(pi.charAt(2));
 	// 4. Print ALL the digits of the Pi String, putting each digit on a new line  (hint: use a loop)
-
+for (int i = 3; i < pi.length(); i++) {
+	//System.out.println(pi.charAt(i));
+}
 	// 5. Skip this step if your computer cannot play sounds. 
 	//		if (canPlaySounds) {
 	//				 Use the speak() method to speak each digit of Pi.   
 	//		}
 	
+
 	
 	// [CHALLENGE]
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" 
-	
-	
+	for (int i = 0; i < pi.length(); i++) {
+		char c = getInputFromUser();
+	if(c==pi.charAt(i)) {
+		JOptionPane.showMessageDialog(null, "correct");
+	}else {
+		JOptionPane.showMessageDialog(null, "incorrect");
+	}
+	}
+	}
 	
 	/********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
 
